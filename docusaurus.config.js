9be3aa7f381792b15b68fc0022c9e2b312d72791
ExistingPerson <164225044+ExistingPerson08@users.noreply.github.com>
2025-11-04 19:33:46 +0000

    Fix wrong base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,7 +5,7 @@ const lightCodeTheme = require("prism-react-renderer/themes/github");
 const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 const organizationName = "Spacefin";
-const projectName = "spacefin-website";
+const projectName = "Spacefin-website";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
